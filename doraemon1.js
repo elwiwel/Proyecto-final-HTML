17db837c4162842bdd12044f51b59da015dadadd
elwiwel <cysgod2@gmail.com>
2025-10-21 14:02:57 +0200

    Página secundaria

--- doraemon1.js
+++ doraemon1.js
@@ -17,3 +17,17 @@ formDoras.forEach(form => {
         form.querySelector("img").style.opacity = 100;
       });
 });
+
+let nombre;
+let aparato;
+const boton = document.getElementById('botonform');
+
+boton.addEventListener('click', function() {
+    nombre = document.getElementById("name").value;
+    alert("¡Gracias por unirte a nuestro club, " + nombre + "!");
+    aparato = document.getElementById("aparato").value;
+    if(aparato === "Menú durum mixto"){
+        alert("¡Estupendo! Solo un verdadero fan del durum podrá unirse a nuestro club.");
+        window.location.href = "secondary.html";
+    }
+})
